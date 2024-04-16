6e4e27117ebd11360bc1b1516c6cd0eae3531a52
Ihor <ihor.stanovyi@gmail.com>
2024-04-16 18:35:07 +0300

    complete 12 final task

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,22 +1,26 @@
+import axios from 'axios';
+
 const API_KEY = '43416172-86a986f8fd379e5189cb30c8e';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export function getImages(query) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-
-  const fetchData = fetch(`${BASE_URL}?${params}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    } else {
-      return response.json();
+export async function getImages(query, page) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page,
+        per_page: 15,
+      },
+    });
+    if (!response.data.hits.length) {
+      throw new Error('No images found');
     }
-  });
-
-  return fetchData;
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
